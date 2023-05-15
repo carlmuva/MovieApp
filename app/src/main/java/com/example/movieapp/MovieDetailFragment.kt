adc171f9345fdb6769a84210ad4d21dddc20ec40
CarlosMurillo <carlmuva@hotmail.com>
2023-05-15 04:21:01 -0600

    UI detail creada

---
+++ app/src/main/java/com/example/movieapp/MovieDetailFragment.kt
@@ -0,0 +1,33 @@
+package com.example.movieapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.movieapp.databinding.FragmentMovieDetailBinding
+
+
+class MovieDetailFragment : Fragment() {
+
+    private lateinit var mBinding : FragmentMovieDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        mBinding = FragmentMovieDetailBinding.inflate(inflater,container,false)
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+}
